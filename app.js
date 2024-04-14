4a123c1be5aa88dfa113ed986349cd5e212b128c
Aslan Bezhanov <bezhanov@sfedu.ru>
2024-04-14 20:10:53 +0300

    add: frontend module

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
